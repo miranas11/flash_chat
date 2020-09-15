f1611cb68139958c05ed7cdb091127737730773c
miranas11 <anasmir24@gmail.com>
2020-09-15 23:20:12 +0530

    Various Changes- -added messagebubbles -add way to receive messages in right order -added flexible widget to icons

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/utilities/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _cloud = FirebaseFirestore.instance;
+User loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -11,17 +15,26 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
-  User loggedInUser;
+  String message;
+  final messageController = TextEditingController();
 
   void getCurrentUser() async {
     try {
       final user = _auth.currentUser;
       if (user != null) {
         loggedInUser = user;
-        print(loggedInUser);
+        print(loggedInUser.email);
       }
     } catch (e) {
       print(e);
+    }
+  }
+
+  void getStreamMessages() async {
+    await for (var snapshot in _cloud.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
     }
   }
 
@@ -52,6 +65,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamMesages(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -59,15 +73,21 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageController,
                       onChanged: (value) {
-                        //Do something with the user input.
+                        message = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _cloud.collection('messages').add({
+                        'text': message,
+                        'sender': loggedInUser.email,
+                        'time': FieldValue.serverTimestamp()
+                      });
+                      messageController.clear();
                     },
                     child: Text(
                       'Send',
@@ -79,6 +99,88 @@ class _ChatScreenState extends State<ChatScreen> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class StreamMesages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _cloud
+          .collection('messages')
+          .orderBy('time', descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final messages = snapshot.data.docs.reversed;
+        List<MessageBubbles> messageBubbles = [];
+        for (var message in messages) {
+          final messageText = message.get('text');
+          final messageSender = message.get('sender');
+          final messageTime = message.get('time') as Timestamp;
+          final messageBubble = MessageBubbles(
+            sender: messageSender,
+            text: messageText,
+            isMe: messageSender == loggedInUser.email,
+            time: messageTime,
+          );
+          messageBubbles.add(messageBubble);
+        }
+        return Expanded(
+          child: ListView(
+            reverse: true,
+            addAutomaticKeepAlives: false,
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MessageBubbles extends StatelessWidget {
+  final String sender;
+  final String text;
+  final bool isMe;
+  final Timestamp time;
+  MessageBubbles({this.sender, this.text, this.isMe, this.time});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
+      child: Column(
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.start : CrossAxisAlignment.end,
+        children: [
+          Text(
+            '$sender',
+            style: TextStyle(color: Colors.black54, fontSize: 9),
+          ),
+          Material(
+            elevation: 5,
+            borderRadius: BorderRadius.only(
+                topLeft: !isMe ? Radius.circular(17) : Radius.circular(0),
+                bottomLeft: Radius.circular(17),
+                bottomRight: Radius.circular(17),
+                topRight: isMe ? Radius.circular(17) : Radius.circular(0)),
+            color: isMe ? Colors.white : Colors.lightBlueAccent,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+              child: Text(
+                '$text',
+                style: TextStyle(
+                    color: isMe ? Colors.black : Colors.white, fontSize: 15),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -30,11 +30,13 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
                 ),
               ),
               SizedBox(
@@ -72,6 +74,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                     setState(() {
                       _isSpining = true;
                     });
+                    // ignore: unused_local_variable
                     final newUser = await _auth.createUserWithEmailAndPassword(
                         email: email, password: password);
                     print('hello');
